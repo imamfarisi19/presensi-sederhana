daaca3eecb4430d17b8a1e7ab51f35ec48ac0a85
imamfarisi19 <edwardindo1@gmail.com>
2021-01-03 23:01:15 +0800

    presensi selesai

--- routes/web.php
+++ routes/web.php
@@ -23,19 +23,19 @@ Route::get('/', function () {
 
 route::get('/registrasi',[LoginController::class,'registrasi'])->name('registrasi');
 route::post('/simpanregistrasi',[LoginController::class,'simpanregistrasi'])->name('simpanregistrasi');
-route::get('/login',[LoginController::class,'halamanLogin'])->name('login');
-route::post('/postLogin',[LoginController::class,'postLogin'])->name('postLogin');
+route::get('/login',[LoginController::class,'halamanlogin'])->name('login');
+route::post('/postlogin',[LoginController::class,'postlogin'])->name('postlogin');
 route::get('/logout',[LoginController::class,'logout'])->name('logout');
 
-Route::group(['middleware' => ['auth','ceklevel:admin,karyawan']], function(){
-    route::get('/home',[HomeController::class,'index'])->name('home');
+Route::group(['middleware' => ['auth','ceklevel:admin,karyawan']], function () {
+    route::get('/home',[HomeController::class,'index'])->name('home');    
 });
 
-Route::group(['middleware' => ['auth','ceklevel:karyawan']], function(){
+Route::group(['middleware' => ['auth','ceklevel:karyawan']], function () {
     route::post('/simpan-masuk',[PresensiController::class,'store'])->name('simpan-masuk');
-    route::get('/presensi-masuk',[PresensiController::class,'index'])->name('presensi-masuk');
-    route::get('/presensi-keluar',[PresensiController::class,'keluar'])->name('presensi-keluar');
+    route::get('/presensi-masuk',[PresensiController::class,'index'])->name('presensi-masuk');    
+    route::get('/presensi-keluar',[PresensiController::class,'keluar'])->name('presensi-keluar');   
     Route::post('ubah-presensi',[PresensiController::class,'presensipulang'])->name('ubah-presensi');
-    Route::post('filter-data',[PresensiController::class,'halamanrekap'])->name('filter-data');
-    Route::post('filter-data/{tglawal}/{tglakhir}',[PresensiController::class,'tampildatakeseluruhan'])->name('filter-data-keseluruhan');
+    Route::get('filter-data',[PresensiController::class,'halamanrekap'])->name('filter-data'); 
+    Route::get('filter-data/{tglawal}/{tglakhir}',[PresensiController::class,'tampildatakeseluruhan'])->name('filter-data-keseluruhan'); 
 });
